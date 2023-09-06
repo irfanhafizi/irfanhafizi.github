e51bbec3ef0a2ad980de4aebb1a620f364723e08
irfanhafizi <irfanhafizi2000@gmail.com>
2023-09-06 21:00:11 +0800

    Portfolio

--- zakat/interviewer/utils.php
+++
@@ -1,8 +0,0 @@
-<?php
-$parameter = $parameter_id;
-$key = $_SESSION['inter_id'];
-$iv = $_SESSION['iv'];
-
-$encrypted_id = openssl_encrypt($parameter, "AES-256-CBC", $key, 0, $iv);
-$encoded_id = base64_encode($encrypted_id . $iv);
-?>
